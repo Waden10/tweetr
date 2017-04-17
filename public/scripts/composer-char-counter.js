4b990fab4bed53c631ee617ce186cae66b75ae1b
waden10 <fantm10@hotmail.com>
2017-04-17 20:19:19 +0000

    need to do this a little more frequently

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  $(".new-tweet form textarea").on("keyup", function(event) {
+    let number = $(this).val().length;
+    let counterLocation = $ (this).parent().find(".counter");
+    let charaCounter = counterLocation.text(140 - number);
+    if (number >  140) {
+      charaCounter.addClass('error');
+    } else {
+      charaCounter.removeClass('error');
+    }
+
+    $(".add-comment-btn").click(function(){
+      $(this).next(".inline-comment-form").slideToggle(200)
+        .find("textarea").focus();
+
+        return false;
+    });
+
+  });
+
+});
